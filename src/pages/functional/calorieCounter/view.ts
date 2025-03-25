4d92e1457eacbc22b1a8941eb76d4e9e61606035
Christian <bournecreative@gmail.com>
2025-03-24 23:17:21 -0500

    clear code from files to start fresh for this example

--- src/pages/functional/calorieCounter/view.ts
+++
@@ -1,9 +0,0 @@
-function view(dispatch, model) {
-    const main = document.createElement('div')
-    const heading = document.createElement('h1')
-    main.setAttribute("class", "mw6 center")
-    heading.setAttribute("class", "f2 pv 2bb")
-    heading.textContent = 'Calorie Counter'
-
-    
-}
